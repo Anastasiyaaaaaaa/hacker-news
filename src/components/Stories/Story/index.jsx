e9ab4dcb7252ed866007d386a4788e441d7e04ff
Anastasiya Soboleva <soboleva@tiburon-research.ru>
2022-11-16 22:14:09 +0300

    минимальные стили для stories

--- src/components/Stories/Story/index.jsx
+++ src/components/Stories/Story/index.jsx
@@ -4,11 +4,14 @@ import './style.css';
 export const Story = ({ story }) => {
 
     return story && <div className='story'>
-        <Link to={`storyPage_${story.id}`}> 
-            <p>by {story.by}</p>
-            <p>time {story.time}</p>
-            <p>score {story.score}</p>
-            <p>comments {story.descendants}</p>
+        <Link to={`storyPage_${story.id}`}>
+            <div className='story-title'>{story.title}</div>
+            <div className='story-by'>by {story.by}</div>
+            <div className='story-bottom'> 
+                <span>time {story.time}</span>
+                <span>score {story.score}</span>
+                <span>comments {story.descendants}</span>
+            </div>
         </Link>
     </div>
 }
